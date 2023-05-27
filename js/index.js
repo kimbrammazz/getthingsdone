07869d2114359cf0322d5119effc0989c68df4ba
Kim Bramwell Mazzarella <kimbm@Kims-MacBook-Pro.local>
2023-05-27 15:06:18 +1000

    start validation

---
+++ js/index.js
@@ -0,0 +1,44 @@
+const newTask = new TaskManager();
+
+const newTaskForm = document.querySelector("#addTaskForm");
+const newTaskName = document.querySelector("#formTask");
+const newTaskStatus = document.querySelector("#formStatus");
+const newTaskAssignee = document.querySelector("#formAssignee");
+const newTaskDueDate = document.querySelector("#formDue");
+const newTaskDescription = document.querySelector("#formDescription");
+
+newTaskForm.addEventListener("submit", (e) => {
+	console.log("in form");
+	e.preventDefault();
+
+	validateInputs();
+
+	console.log(taskName);
+
+	newTask.addTask(
+		taskName,
+		taskAssignee,
+		taskDueDate,
+		taskDescription,
+		taskStatus
+	);
+
+	console.log(newTask.tasks);
+	newTaskName.value = "";
+	newTaskAssignee.value = "";
+	newTaskDueDate.value = "";
+	newTaskDescription.value = "";
+	newTaskStatus.value = "";
+});
+
+// validation
+const showError = (input, message) => {
+	const formField = input.parentElement;
+};
+const validateInputs = () => {
+	const taskName = newTaskName.value;
+	const taskAssignee = newTaskAssignee.value;
+	const taskDueDate = newTaskDueDate.value;
+	const taskDescription = newTaskDescription.value;
+	const taskStatus = newTaskStatus.value;
+};
---
+++ js/taskManager.js
@@ -0,0 +1,24 @@
+class TaskManager {
+	constructor(currentId = 0) {
+		this.tasks = [];
+		this.currentId = currentId;
+	}
+
+	addTask(
+		taskName,
+		taskAssignee,
+		taskDueDate,
+		taskDescription,
+		taskStatus = "todo"
+	) {
+		const newTask = {
+			taskName: taskName,
+			taskAssignee: taskAssignee,
+			taskDueDate: taskDueDate,
+			taskDescription: taskDescription,
+			taskStatus: taskStatus,
+			id: this.currentId++,
+		};
+		return this.tasks.push(newTask);
+	}
+}
